6843d520a87510ccf955e6f462914887dd14bba4
theblackturtle <tuan.tv1966@sinhvien.hoasen.edu.vn>
2020-12-19 17:27:16 +0700

    add whitelist flag and improve linkfinder

--- core/robots.go
+++ core/robots.go
@@ -1,48 +1,49 @@
 package core
 
 import (
-	"fmt"
-	"github.com/gocolly/colly/v2"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strings"
-	"sync"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "regexp"
+    "strings"
+    "sync"
+
+    "github.com/gocolly/colly/v2"
 )
 
 func ParseRobots(site *url.URL, output *Output, c *colly.Collector, wg *sync.WaitGroup) {
-	defer wg.Done()
-	robotsURL := site.String() + "/robots.txt"
+    defer wg.Done()
+    robotsURL := site.String() + "/robots.txt"
 
-	resp, err := http.Get(robotsURL)
-	if err != nil {
-		return
-	}
-	if resp.StatusCode == 200 {
-		Logger.Infof("Found robots.txt: %s", robotsURL)
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return
-		}
-		lines := strings.Split(string(body), "\n")
+    resp, err := http.Get(robotsURL)
+    if err != nil {
+        return
+    }
+    if resp.StatusCode == 200 {
+        Logger.Infof("Found robots.txt: %s", robotsURL)
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            return
+        }
+        lines := strings.Split(string(body), "\n")
 
-		var re = regexp.MustCompile(".*llow: ")
-		for _, line := range lines {
-			if strings.Contains(line, "llow: ") {
-				url := re.ReplaceAllString(line, "")
-				url = FixUrl(url, site)
-				if url == "" {
-					continue
-				}
-				outputFormat := fmt.Sprintf("[robots] - %s", url)
-				fmt.Println(outputFormat)
-				if output != nil {
-					output.WriteToFile(outputFormat)
-				}
-				_ = c.Visit(url)
-			}
-		}
-	}
+        var re = regexp.MustCompile(".*llow: ")
+        for _, line := range lines {
+            if strings.Contains(line, "llow: ") {
+                url := re.ReplaceAllString(line, "")
+                url = FixUrl(site, url)
+                if url == "" {
+                    continue
+                }
+                outputFormat := fmt.Sprintf("[robots] - %s", url)
+                fmt.Println(outputFormat)
+                if output != nil {
+                    output.WriteToFile(outputFormat)
+                }
+                _ = c.Visit(url)
+            }
+        }
+    }
 
 }
--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/onsi/ginkgo v1.14.2 // indirect
+	github.com/onsi/gomega v1.10.4 // indirect
 	github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
-	golang.org/x/net v0.0.0-20200822124328-c89045814202
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
 )
